04e4add37dc5da970e6e995a13cee138744cf973
parimalroy <parimal.tbd@gmail.com>
2025-05-06 01:02:03 +0600

    subcategory blade create

---
+++ resources/views/Backend/subcate;gory/create.blade.php
@@ -0,0 +1,75 @@
+@extends('Backend.layout.app')
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid my-2">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Create Sub Category</h1>
+                    <h6 id="message"></h6>
+                </div>
+                <div class="col-sm-6 text-right">
+                    <a href="{{ route('admin.dashboard') }}" class="btn btn-primary">Back</a>
+                </div>
+            </div>
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- Main content -->
+    <section class="content">
+        <!-- Default box -->
+        <div class="container-fluid">
+            <form id="categoryForm">
+                @csrf
+                <div class="card">
+                    <div class="card-body">
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="name">Name</label>
+                                    <input type="text" id="name" class="form-control" placeholder="Name"
+                                        name="name">
+                                    <p></p>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="email">Slug</label>
+                                    <input type="text" id="slug" class="form-control" placeholder="Slug"
+                                        name="slug">
+                                    <p></p>
+                                </div>
+                            </div>
+                            {{-- <div class="col-md-6">
+                            <div class="mb-3">
+                                <label for="email">Photo</label>
+                                <input type="file" name="slug" id="slug" class="form-control" placeholder="Slug">
+                            </div>
+                        </div> --}}
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="status">Status</label>
+                                    <select class="form-control" id="status" name="status">
+                                        <option value="active">Active</option>
+                                        <option value="inactive">Inactive</option>
+                                    </select>
+                                    <p></p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="pb-5 pt-3">
+                    <button id="submitBtn" class="btn btn-primary">Create</button>
+                    <a href="brands.html" class="btn btn-outline-dark ml-3">Cancel</a>
+                </div>
+        </div>
+        </form>
+        <!-- /.card -->
+    </section>
+    <!-- /.content -->
+@endsection
